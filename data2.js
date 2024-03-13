0f2c1e697abb06ae9afede92e1a6e80ad9630314
Shah685 <163100469+Shah685@users.noreply.github.com>
2024-03-13 10:54:10 +0000

    done

---
+++ data2.js
@@ -0,0 +1,6 @@
+let score= "12abachhj"
+console.log(typeof score);
+console.log((typeof score));
+let valueInString  = string (score)
+console.log((typeof valueInString));
+console.log(valueInString);
